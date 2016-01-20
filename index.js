49a6a4ba2332739d10fadd6b5ee62c98ba213837
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-01-20 13:20:56 +0100

    use proper path

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
+var path = require('path')
 var fs = require('fs')
 var through = require('through2')
 var chunk = require('chunk-stream')
 
-fs.createReadStream('data')
+fs.createReadStream(path.join(__dirname, 'data'))
   .pipe(chunk(100))
   .pipe(through(function (chunk, enc, cb) {
     this.push(chunk)
